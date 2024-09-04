d6164f33a502e0239d874b98f4239c2e53c44b74
Wendy <lovelyalexzhou@outlook.com>
2024-09-04 11:21:58 +1000

    Initial Commit

---
+++ wk5-weather/wk5_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  wk5_weatherApp.swift
+//  wk5-weather
+//
+//  Created by Wendy Zhou on 4/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct wk5_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
