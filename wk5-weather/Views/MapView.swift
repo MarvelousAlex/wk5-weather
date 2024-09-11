be9a2d6b4acaf14c5466f15077eed2567ce64cd3
Wendy <lovelyalexzhou@outlook.com>
2024-09-11 12:12:30 +1000

    add sth...

---
+++ wk5-weather/Views/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  wk5-weather
+//
+//  Created by Wendy Zhou on 11/9/2024.
+//
+
+import Foundation
+import  SwiftUI
+
+struct MapView: View {
+	
+	@EnvironmentObject var viewModel: WeatherViewModel
+	
+	var body: some View{
+		Text("Hi")
+	}
+	
+}
+
